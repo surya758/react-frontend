51c1bde62dc084b96518ab77d7c04607756cef2e
Surya kant <skant758@gmail.com>
2022-07-11 00:01:43 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
 					Edit <code>src/App.js</code> and save to reload.
 				</p>
 				<p>
-					This is the <span>new</span> content
+					This is the <span>new</span> content of the day
 				</p>
 				<a
 					className='App-link'
